fd09bfd3e1b6ba333f19f4c3e2c1bd77e45ca761
mima <bhbstar.me@gmail.com>
2018-11-23 10:45:42 +0900

    refactor and fix js

--- app/assets/javascripts/circle.js
+++ app/assets/javascripts/circle.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     function isLiked(){
         //サークル番号を取得
-        var id = $('#circle_id').text();
+        const id = $('#circle_id').text();
         console.log(id);
         //ブラウザに保存されているidを取得
-        var userKeepId = localStorage.getItem("user");
+        const userKeepId = localStorage.getItem("user");
         console.log(userKeepId);
         
         //保存されてない場合そのまま
@@ -12,18 +12,17 @@ $(document).ready(function(){
             return;
         }
         
-        
         if(userKeepId.match(String(id))){
             console.log("like");
             $('#like').html("<i class='fa fa-heart mr-2'></i>お気に入り解除");
-            $('#like').css('background-color','#FF5964');
-            $('#like').css('border-color','#FF5964');
+            // $('#like').css('background-color','#FF5964');
+            // $('#like').css('border-color','#FF5964');
         }else{
             //このページのIdは保存されていない
             console.log("dislike");
             $('#like').html("<i class='fa fa-heart mr-2'></i>お気に入り");
-            $('#like').css('background-color','#F5BB1C');
-            $('#like').css('border-color','#F5BB1C');
+            // $('#like').css('background-color','#F5BB1C');
+            // $('#like').css('border-color','#F5BB1C');
         }
 
     }
@@ -31,33 +30,30 @@ $(document).ready(function(){
 
     $('#like').click(function(){
         //サークル番号を取得
-        var id = $('#circle_id').text();
+        const id = $('#circle_id').text();
         console.log(id);
         //ブラウザに保存されているidを取得
-        var userKeepId = localStorage.getItem("user");
+        const userKeepId = localStorage.getItem("user");
         console.log(userKeepId);
 
         if(userKeepId&&!userKeepId.match(String(id))){
             //ブラウザにすでにuserKeepIdが保存されていて、かつこのページのIdは保存されていない
             localStorage.setItem("user",userKeepId+id);
-            this.innerHTML="<i class='fa fa-trash mr-2'></i>お気に入り解除";
-            this.style.backgroundColor='#FF5964';
-            this.style.borderColor='#FF5964';
+            $('#like').html("<i class='fa fa-trash mr-2'></i>お気に入り解除");
+            // this.style.backgroundColor='#FF5964';
+            // this.style.borderColor='#FF5964';
         }else if(!userKeepId){
             //ブラウザにuserKeepIdが保存されていない
             localStorage.setItem("user",id);
-            this.innerHTML="<i class='fa fa-trash mr-2'></i>お気に入り解除";
-            this.style.backgroundColor='#FF5964';
-            this.style.borderColor='#FF5964';
+            $('#like').html("<i class='fa fa-trash mr-2'></i>お気に入り解除");
+            // this.style.backgroundColor='#FF5964';
+            // this.style.borderColor='#FF5964';
         }else{
             var userKeepIdDel = userKeepId.replace(String(id),"");
             localStorage.setItem("user",userKeepIdDel);
-            this.innerHTML="<i class='fa fa-heart mr-2'></i>お気に入り";
-            this.style.backgroundColor='#f5bb1c';
-            this.style.borderColor='#f5bb1c';
+            $('#like').html("<i class='fa fa-heart mr-2'></i>お気に入り");
+            // this.style.backgroundColor='#f5bb1c';
+            // this.style.borderColor='#f5bb1c';
         }
-
-
     });
-
 });
